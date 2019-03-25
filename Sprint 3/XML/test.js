490a63fe2bad42729f90edb1d87a3c80f701701e
Alfa-Valor <45150784+Alfa-Valor@users.noreply.github.com>
2019-03-25 09:02:49 +0100

    FUCK

--- Sprint 3/XML/test.js
+++ Sprint 3/XML/test.js
@@ -10,20 +10,9 @@ function load(url, callback) {
     xobj.send(null);
 }
 
-// request.send();
 
 load("voorbeeld.xml", function (response) {
     var file = response;
     console.log(file);
     document.getElementById("test").innerHTML = file;
-    // for(var i = 0; i < file.length; i++){
-    //     file = file.replace(",", " ")
-    // }
-    // file = file.split(/\r\n|\n/);
-    //     console.log(file[0]);
-    //         console.log(file);
-    //         for(var i = 0; i < file.length; i++){
-    //             document.getElementById("input").innerHTML+= "<h4 id='heck'>" + file[i] +"</h4>"  + "<br>";
-    //         }
-           
 })
